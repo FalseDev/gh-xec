1132f8061b27108446c0dbb39c675caed368fbbd
Navaneethan <59121948+FalseDev@users.noreply.github.com>
2023-03-09 10:15:23 +0000

    Cleanup and reorganize

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 #![warn(clippy::pedantic)]
 #![warn(clippy::nursery)]
 
+mod releases;
+use crate::releases::Releases;
 use clap::{Args, Parser, Subcommand};
-use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use serde::de::DeserializeOwned;
 use std::{
     env,
     io::{Cursor, Write},
@@ -11,7 +13,12 @@ use std::{
 
 macro_rules! fzf {
     ($vector:expr, $field: ident) => {{
-        let choice = fzf_select($vector.iter().map(|a| a.$field.to_owned()).collect());
+        let choice = fzf_select(
+            &$vector
+                .iter()
+                .map(|a| a.$field.to_owned())
+                .collect::<Vec<_>>(),
+        );
         $vector
             .into_iter()
             .filter(|a| a.$field == choice)
@@ -66,7 +73,7 @@ fn install(args: &InstallArgs) {
         .unwrap()
         .wait()
         .unwrap();
-    if let Some((_, ext)) = asset.name.rsplit_once(".") {
+    if let Some((_, ext)) = asset.name.rsplit_once('.') {
         match ext {
             "deb" => install_deb(&asset.name),
             "tar" | "gz" | "tgz" => extract_tar(&asset.name),
@@ -86,7 +93,7 @@ fn extract_zip(filename: &str) {
 }
 
 fn extract_tar(filename: &str) {
-    println!("Extracting {} using tar", filename);
+    println!("Extracting {filename} using tar");
     SysCommand::new("tar")
         .args(["--extract", "--file", filename, "--one-top-level"])
         .spawn()
@@ -96,7 +103,7 @@ fn extract_tar(filename: &str) {
 }
 
 fn install_deb(filename: &str) {
-    println!("Installing {} using dpkg", filename);
+    println!("Installing {filename} using dpkg");
     SysCommand::new("sudo")
         .args(["dpkg", "--install", filename])
         .spawn()
@@ -105,7 +112,7 @@ fn install_deb(filename: &str) {
         .unwrap();
 }
 
-fn gh_api<'a, T>(mut args: Vec<String>) -> T
+fn gh_api<T>(mut args: Vec<String>) -> T
 where
     T: DeserializeOwned,
 {
@@ -117,7 +124,7 @@ where
     serde_json::from_reader(Cursor::new(output.stdout)).unwrap()
 }
 
-fn fzf_select(choices: Vec<String>) -> String {
+fn fzf_select(choices: &[String]) -> String {
     let mut fzf_proc = SysCommand::new("fzf")
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
@@ -133,160 +140,4 @@ fn fzf_select(choices: Vec<String>) -> String {
     String::from_utf8_lossy(&output.stdout)
         .trim_end()
         .to_string()
-}
-
-pub type Releases = Vec<Release>;
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Release {
-    pub url: String,
-    #[serde(rename = "assets_url")]
-    pub assets_url: String,
-    #[serde(rename = "upload_url")]
-    pub upload_url: String,
-    #[serde(rename = "html_url")]
-    pub html_url: String,
-    pub id: i64,
-    pub author: Author,
-    #[serde(rename = "node_id")]
-    pub node_id: String,
-    #[serde(rename = "tag_name")]
-    pub tag_name: String,
-    #[serde(rename = "target_commitish")]
-    pub target_commitish: String,
-    pub name: String,
-    pub draft: bool,
-    pub prerelease: bool,
-    #[serde(rename = "created_at")]
-    pub created_at: String,
-    #[serde(rename = "published_at")]
-    pub published_at: String,
-    pub assets: Vec<Asset>,
-    #[serde(rename = "tarball_url")]
-    pub tarball_url: String,
-    #[serde(rename = "zipball_url")]
-    pub zipball_url: String,
-    pub body: String,
-    pub reactions: Option<Reactions>,
-    #[serde(rename = "mentions_count")]
-    pub mentions_count: Option<i64>,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Author {
-    pub login: String,
-    pub id: i64,
-    #[serde(rename = "node_id")]
-    pub node_id: String,
-    #[serde(rename = "avatar_url")]
-    pub avatar_url: String,
-    #[serde(rename = "gravatar_id")]
-    pub gravatar_id: String,
-    pub url: String,
-    #[serde(rename = "html_url")]
-    pub html_url: String,
-    #[serde(rename = "followers_url")]
-    pub followers_url: String,
-    #[serde(rename = "following_url")]
-    pub following_url: String,
-    #[serde(rename = "gists_url")]
-    pub gists_url: String,
-    #[serde(rename = "starred_url")]
-    pub starred_url: String,
-    #[serde(rename = "subscriptions_url")]
-    pub subscriptions_url: String,
-    #[serde(rename = "organizations_url")]
-    pub organizations_url: String,
-    #[serde(rename = "repos_url")]
-    pub repos_url: String,
-    #[serde(rename = "events_url")]
-    pub events_url: String,
-    #[serde(rename = "received_events_url")]
-    pub received_events_url: String,
-    #[serde(rename = "type")]
-    pub type_field: String,
-    #[serde(rename = "site_admin")]
-    pub site_admin: bool,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Asset {
-    pub url: String,
-    pub id: i64,
-    #[serde(rename = "node_id")]
-    pub node_id: String,
-    pub name: String,
-    pub label: String,
-    pub uploader: Uploader,
-    #[serde(rename = "content_type")]
-    pub content_type: String,
-    pub state: String,
-    pub size: i64,
-    #[serde(rename = "download_count")]
-    pub download_count: i64,
-    #[serde(rename = "created_at")]
-    pub created_at: String,
-    #[serde(rename = "updated_at")]
-    pub updated_at: String,
-    #[serde(rename = "browser_download_url")]
-    pub browser_download_url: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Uploader {
-    pub login: String,
-    pub id: i64,
-    #[serde(rename = "node_id")]
-    pub node_id: String,
-    #[serde(rename = "avatar_url")]
-    pub avatar_url: String,
-    #[serde(rename = "gravatar_id")]
-    pub gravatar_id: String,
-    pub url: String,
-    #[serde(rename = "html_url")]
-    pub html_url: String,
-    #[serde(rename = "followers_url")]
-    pub followers_url: String,
-    #[serde(rename = "following_url")]
-    pub following_url: String,
-    #[serde(rename = "gists_url")]
-    pub gists_url: String,
-    #[serde(rename = "starred_url")]
-    pub starred_url: String,
-    #[serde(rename = "subscriptions_url")]
-    pub subscriptions_url: String,
-    #[serde(rename = "organizations_url")]
-    pub organizations_url: String,
-    #[serde(rename = "repos_url")]
-    pub repos_url: String,
-    #[serde(rename = "events_url")]
-    pub events_url: String,
-    #[serde(rename = "received_events_url")]
-    pub received_events_url: String,
-    #[serde(rename = "type")]
-    pub type_field: String,
-    #[serde(rename = "site_admin")]
-    pub site_admin: bool,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Reactions {
-    pub url: String,
-    #[serde(rename = "total_count")]
-    pub total_count: i64,
-    #[serde(rename = "+1")]
-    pub n1: i64,
-    #[serde(rename = "-1")]
-    pub n12: i64,
-    pub laugh: i64,
-    pub hooray: i64,
-    pub confused: i64,
-    pub heart: i64,
-    pub rocket: i64,
-    pub eyes: i64,
 }
